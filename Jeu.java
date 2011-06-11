8146b24b950c73270ac64d29578e8c02e7323212
theblues <erwan76.lebrun@gmail.com>
2011-06-11 14:47:08 +0200

    Modif plus facile au début a compiler

--- Jeu.java
+++ Jeu.java
@@ -3,24 +3,24 @@ import java.util.Collections;
 
 public class Jeu
 {
-    private ArrayList<Cube> sac = new ArrayList<Cube> ();
-    private ArrayList<Ballon> pioche = new ArrayList<Ballon>();
-    private ArrayList<Ballon> defausse = new ArrayList<Ballon>();
-    private ArrayList<Ballon> main = new ArrayList<Ballon>();
+    private static ArrayList<Cube> sac = new ArrayList<Cube> ();
+    private static ArrayList<Ballon> pioche = new ArrayList<Ballon>();
+    private static ArrayList<Ballon> defausse = new ArrayList<Ballon>();
+    private static ArrayList<Ballon> main = new ArrayList<Ballon>();
     
-    private final int NBCARTE = 45;
-    private final int NBCUBE = 45;
+    private static final int NBCARTE = 45;
+    private static final int NBCUBE = 45;
     
-    private int[][] tabBallon = {   { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },         // rouge
-                                    { 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1 },         // jaune
-                                    { 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1 },         // vert
-                                    { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },         // bleu
-                                    { 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1 },         // gris
-                                };
+    private static int[][] tabBallon = {   { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },         // rouge
+                                           { 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1 },         // jaune
+                                           { 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1 },         // vert
+                                           { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },         // bleu
+                                           { 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1 },         // gris
+                                       };
                                
                                
     // on initialise la pioche dans l'ordre
-    public void initialiserPicohe()
+    public static void initialiserPioche()
     {
         for (int i = 1; i <= tabBallon.length;  i++)
             if ( tabBallon[i-1][0] == 1)
@@ -43,19 +43,21 @@ public class Jeu
                 pioche.add (new Ballon (i, "Gris"));                
     }
     
-    public void melangerPioche()
+    public static void melangerPioche()
     {
         Collections.shuffle(pioche);
     }
     
-    public void donnerCarte()
+    public static void donnerCarte()
     {
-    
+        /*
         for (int i = 0, int j = pioche.size()-1 ; i < 8; ++i, --j)
         {
             main.add (pioche
+            */
+    }
     
-    public void initialiserSac()
+    public static void initialiserSac()
     {
         for (int i = 0; i < 13; ++i)
             sac.add (new Cube ("Rouge"));
@@ -74,14 +76,20 @@ public class Jeu
             
     }
     
-    public void melangerSac()
+    public static void melangerSac()
     {
         Collections.shuffle(sac);
     }
     
-    public void defausseVersPioche()
+    public static void defausseVersPioche()
     {
-        pioche = defausse.clone();
+        //pioche = defausse.clone(); bug
         defausse.clear();
     }
-}
+    
+    public static void main (String[] args)
+    {
+        initialiserPioche();
+        for ( Ballon b: pioche ) System.out.println (b); // manque le toString xD
+    }
+}       
